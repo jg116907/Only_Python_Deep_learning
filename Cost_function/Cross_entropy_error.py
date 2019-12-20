f5b8ec99036575923db998d31c66b0ad042382a7
jg116907 <jg116907@naver.com>
2019-12-20 17:42:19 +0900

    Fixed cross entropy error function

--- Cost_function/Cross_entropy_error.py
+++ Cost_function/Cross_entropy_error.py
@@ -15,6 +15,8 @@ def cee_batch(y,t): # 정답 값으로 y가 들어올경우
   if y.dim == 1:
     t = t.reshape(1,t.size)
     y = y.reshape(1,y.size)
+  if t.size == y.size: # 훈련 데이터가 one-hot encoding 된 상태일 경우 정답 레이블의 index로 변경
+    t = t.argmax(axis=1)
   batch_size = y.shape[0]
   return -np.sum(np.log(y[np.arange(batch_size),t]+1e-7))/batch_size
 
