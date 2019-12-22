6f81251c079a195629608f0933421304b3569718
jg116907 <jg116907@naver.com>
2019-12-22 22:23:08 +0900

    Fixed softmax

--- Activation_function/Softmax.py
+++ Activation_function/Softmax.py
@@ -7,11 +7,20 @@ def softmax_overflow(a):
   return exp_a/sum_exp_a
 
 # exp내의 배열을 배열 내 최대값을 뺴준 값으로 계산 -> overflow 방지
-def softmax(a):
-  c = np.max(a)
-  exp_a = np.exp(a-c)
-  sum_exp_a = sum(exp_a)
-  return exp_a/sum_exp_a
+# def softmax(a): # 1d
+#   c = np.max(a)
+#   exp_a = np.exp(a-c)
+#   sum_exp_a = sum(exp_a)
+#   return exp_a/sum_exp_a
+
+def softmax(x): # 2d
+  if x.ndim == 2:
+    x = x.T
+    x = x - np.max(x, axis=0)
+    y = np.exp(x) / np.sum(np.exp(x), axis=0)
+    return y.T 
+  x = x - np.max(x) # 오버플로 대책
+  return np.exp(x) / np.sum(np.exp(x))
 
 ## test
 # x = np.arange(-10,10,1)
